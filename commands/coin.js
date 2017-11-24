4ece2cbd7376c7285593872bb870d1f5266cf4e2
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-24 15:37:16 -0500

    + Coin, + Guildinfo

---
+++ commands/coin.js
@@ -0,0 +1,8 @@
+module.exports.run = (client, message, args) => {
+    var random = Math.floor((Math.random() * 10) + 1);
+    if(random & 1){
+      message.channel.send("The coin landed on **heads**") // ODD
+    } else {
+      message.channel.send("The coin landed on **tails**") // EVEN
+      }
+};
---
+++ commands/guildinfo.js
@@ -0,0 +1,22 @@
+module.exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+    
+    if(message.channel.type === "dm"){
+        message.channel.send(":x: This command will not work in DM's");
+        return;
+    } else {
+        var guildCreatedAt = new Date(message.guild.createdTimestamp);
+        message.channel.send(new Discord.RichEmbed()
+        .setColor(7506394)
+        .setThumbnail(message.guild.iconURL)
+        .setAuthor(`Information on ${message.guild.name}:`, message.guild.iconURL, null)
+        .addField("Guild Owner:", message.guild.owner.user.tag, true)
+        .addField("Guild ID:", message.guild.id, true)
+        .addField("Members:", message.guild.memberCount, true)
+        .addField("Channels:", `${message.guild.channels.size} channels`, true)
+        .addField("Region:", message.guild.region, true)
+        .addField("Server Created:", guildCreatedAt.toLocaleString(), true)
+        .addField("Emotes:", `${message.guild.emojis.size} emotes`, true)
+    );
+    }
+};

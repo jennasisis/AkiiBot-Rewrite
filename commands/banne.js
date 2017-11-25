2005ac7bf5a5ecf61ad44fc5d6e2ff9bb07d3c2e
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-24 21:22:42 -0500

    renamed bean to banne

---
+++ commands/banne.js
@@ -0,0 +1,10 @@
+module.exports.run = (client, message, args) => {
+    console.log("banne command run");
+    if(message.mentions.users.size < 1){
+        message.channel.send("<:ecks:383800708841078785> Yoo did no menshin sum1");
+    } else if(message.mentions.users.size > 1){
+        message.channel.send("<:ecks:383800708841078785> Yor menshuning 2 meny pepol!");
+    } else {
+        message.channel.send(`<:whte_chck_mrk:383763992981667855> ${message.mentions.users.first()} has bee banne~!!1!`);
+    }
+};

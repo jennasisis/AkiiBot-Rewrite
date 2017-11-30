a0739687aab6518291acfa8b39798bff25280e80
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-30 15:48:40 -0500

    small change / renamed suggestion -> suggest

---
+++ commands/suggest.js
@@ -0,0 +1,14 @@
+module.exports.run = (client, message, args) => {
+    console.log("suggestion command ran");
+    const Discord = require('discord.js');
+    const config = require('../config.json');
+
+    const [arg, ...rest] = args.splice(1);
+    client.users.find("id", "107599228900999168").send(new Discord.RichEmbed()
+        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setTitle("Suggestion:")
+        .setDescription(message.content.substring(config.prefix + 8))
+        .setColor(7506394)
+    );
+    message.channel.send(":white_check_mark: Suggestion sent");
+};

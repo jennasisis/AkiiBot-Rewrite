64be9637a49e592475b09315d36e70a10bdaf38a
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-12-30 18:55:35 -0500

    small edit to info

--- commands/info.js
+++ commands/info.js
@@ -14,7 +14,7 @@ module.exports.run = (client, message, args) => {
         .addField("Invite:", "[Click Here](https://discordapp.com/oauth2/authorize?permissions=2146954487&scope=bot&client_id=336521813926346763)", true)
         .addField("Discord:", "[Click Here](http://discord.gg/54fVgRw)", true)
         .addField("Contributors:", "`Akii#2111` - The bot dev. Duh\n`Gallium#1327` - Helped me with most of this code. Also my good friend.")
-        .addField("Honorable Mentions:", "`OGNovuh#0014` - Added the Fight command, which I no longer use.. Sorry Novuh. ;_;")
+        .addField("Honorable Mentions:", "`OGNovuh#0014` - Added the Fight command, which I no longer use.. Sorry Novuh. ;_;\n[`An Idiot's Guide`](http://anidiots.guide/) - Command handler, eval command, and many other things. Thank you *so* much.")
         .setAuthor("AkiiBot", client.user.avatarURL, "http://discord.gg/54fVgRw")
         .setThumbnail(client.user.avatarURL)
         .setColor(7506394)

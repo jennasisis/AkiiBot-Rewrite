70c565f8f2357d800a899ffb8db3e4f5bcbf5998
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-04 11:08:47 -0500

    added makeshift help command

---
+++ commands/help.js
@@ -0,0 +1,3 @@
+module.exports.run = (client, message, args) => {
+    message.channel.send("Lol so Akii still hasn't made a help command because this bot is actually a rewrite of the original Akiibot. There isnt much here, it's mostly just the tools you would find on any other bot plus some meme commands \*shrug\*");
+}

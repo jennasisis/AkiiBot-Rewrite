29abb49e0ee0c6e2c3f07c48508b01b242cc4df9
cole <me@colexd.us>
2018-01-12 23:06:54 -0800

    improve commands/suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -3,8 +3,10 @@ const RichEmbed = require('discord.js').RichEmbed;
 module.exports.run = (client, message, args) => {
     console.log('suggestion command ran');
 
+    const user = message.author;
+
     client.users.find('id', '107599228900999168').send(new RichEmbed()
-        .setAuthor(message.author.tag, message.author.avatarURL)
+        .setAuthor(user.tag, user.avatarURL)
         .setTitle('Suggestion:')
         .setDescription(message.content.substring(require('../config.json').prefix + 8))
         .setColor(7506394)

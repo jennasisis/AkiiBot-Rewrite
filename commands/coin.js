9bf353ab99feaad06f04f47ffa7ec5eed87ea9be
cole <me@colexd.us>
2018-01-12 22:19:30 -0800

    improve commands/coin.js

--- commands/coin.js
+++ commands/coin.js
@@ -1,9 +1,5 @@
 module.exports.run = (client, message, args) => {
   console.log('coin command ran');
-  if (Math.floor((Math.random() * 10) + 1) & 1) {
-    message.channel.send('The coin landed on **heads**') // ODD
-  }
-  else {
-    message.channel.send('The coin landed on **tails**') // EVEN
-  }
+  if (Math.floor((Math.random() * 10) + 1) & 1) message.channel.send('The coin landed on **heads**') // ODD
+  else message.channel.send('The coin landed on **tails**') // EVEN
 };

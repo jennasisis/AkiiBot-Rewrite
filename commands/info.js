f49d990442d13ab5d520ab434048b6ea8f751a2c
cole <me@colexd.us>
2018-01-12 23:02:11 -0800

    improve commands/info.js

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
     console.log('info command ran')
     const shard = new Discord.ShardClientUtil(client);
+    const avatarUrl = client.user.avatarURL;
+
     message.react('✅');
     message.author.send(new Discord.RichEmbed()
         .addField('Version', require('../package.json').version, true)
@@ -15,8 +17,8 @@ module.exports.run = (client, message, args) => {
         .addField('Discord:', '[Click Here](http://discord.gg/54fVgRw)', true)
         .addField('Contributors:', '`Akii#2111` - The bot dev. Duh\n`Gallium#1327` - Helped me with most of this code. Also my good friend.')
         .addField('Honorable Mentions:', "`OGNovuh#0014` - Added the Fight command, which I no longer use.. Sorry Novuh. ;_;\n[`An Idiot's Guide`](http://anidiots.guide/) - Command handler, eval command, and many other things. Thank you *so* much.")
-        .setAuthor('AkiiBot', client.user.avatarURL, 'http://discord.gg/54fVgRw')
-        .setThumbnail(client.user.avatarURL)
+        .setAuthor('AkiiBot', avatarUrl, 'http://discord.gg/54fVgRw')
+        .setThumbnail(avatarUrl)
         .setColor(7506394)
     )
 };
